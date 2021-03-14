0e2dc94d963b91dc32ba443778dc24ebbdcf9af5
winniediana4 <deinawinnie84@gmail.com>
2021-03-14 18:02:07 +0300

    add README

---
+++ README.md
@@ -0,0 +1,27 @@
+# Dairy Business
+#### A milk production and sales' monitor {14/03/2021}
+#### Author
+Winnie Diana Anyango
+## Description
+This is a program that monitors the production and sales of milk and how much earnings are gotten daily,weekly and yearly.
+### Prerequisites
+You need to have git installed
+You can install it with the following command in your terminal
+`$ sudo apt install git-all`
+### Setup
+To access this project on your local files, you can clone it using these steps
+1. Open your terminal
+1. Use this command to clone `$ git clone 
+1. This will clone the repositoty into your local folder
+### Live Site
+View [live]()
+## Known Bugs
+{No known bugs.}
+## Technologies Used
+1. HTML
+1. CSS
+1. Git
+## Support and contact details
+For inquiries or contributions, you can reach me at deinawinnie84@gmail.com.
+### License
+This project is under the  [MIT](LICENSE) license
--- index.js
+++ index.js
@@ -7,14 +7,15 @@ var production = [a, b , c, d];
 var i=0;
 while (i<production.length){
     var shed = ["A", "B", "C", "D"];
-    console.log("Your production in Shed " + shed[i] + " is " + production[i] + " Litres per day.");
+    console.log("Your production in Shed " + shed[i] + " is " + production[i] + " Litres per day");
     i++;
 }
 var totalProduction = a + b + c + d;
-console.log("Your total production is " + totalProduction + " litres per day.");
+console.log("Your total production is " + totalProduction + " litres per day");
 
 var sellingPrice = 45;
-console.log("Your weekly income will be Ksh " + totalProduction * sellingPrice * 7);
+console.log("Your daily total income will be Ksh. " + totalProduction * sellingPrice)
+console.log("Your weekly income will be Ksh. " + totalProduction * sellingPrice * 7);
 
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 var daysOfMonthLeap = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
